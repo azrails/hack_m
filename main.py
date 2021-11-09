c708c9604ae61bfcf6844eb725fa9be380c9d8af
azrail <wsallei@student.21-school.ru>
2021-11-09 16:59:37 +0300

    11

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import parser
 import os, os.path
 import tkinter as tk
+from tkinter import filedialog as fd
 
 class MainApp(tk.Tk):
     def __init__(self) ->None:
@@ -8,9 +9,11 @@ class MainApp(tk.Tk):
         self.geometry('{}x{}+{}+{}'.format(round(self.winfo_screenwidth() / 1.5), round(self.winfo_screenheight() / 1.5), (self.winfo_screenwidth()-self.winfo_screenwidth()//2)// 2, (self.winfo_screenheight()-self.winfo_screenheight() // 2)// 2))
         self.title('Рассчет службы дорожного покрытия')
         self.frame = tk.Frame()
-        self.btn_chose_directory = tk.Button(text='Chose directory', width=30, height=15, master=self.frame).grid(row=0, column=0, padx=10, pady=10)
+        self.btn_chose_directory = tk.Button(text='Chose directory', width=30, height=15, master=self.frame, command=self.chose_directory).grid(row=0, column=0, padx=10, pady=10)
         self.frame.pack()
-        
+    
+    def chose_directory(self):
+        filename = fd.askopenfilename()
 
 def main():
     app = MainApp()
