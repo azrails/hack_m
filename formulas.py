9cc456a1ca01aa11b02e6d4f9df474655ed2212f
zzx26 <zzx26@mail.ru>
2021-11-09 14:07:27 +0300

    added more formulas

--- formulas.py
+++ formulas.py
@@ -5,6 +5,18 @@ alpha = np.array([0.005, 0.2, 0.7, 1.25, 0.7, 1.5])
 
 # расчет 1
 
+def f_n(a):
+    r = 0.3
+    if a == 1:
+        r = 1
+    elif a == 2:
+        r = 0.55
+    elif a == 3:
+        r = 0.5
+    elif a == 4 or a == 5:
+        r = 0.35
+    return r
+
 def T_oct(q, X, gamma, omega, N_fp):
     return 1 / (np.log10(q)) * np.log10(10 ** X * (q - 1) / (gamma * omega * N_fp * q) + 1)
 
@@ -41,3 +53,18 @@ def Formula_1_05(N_fp, q, delta_t):
 def Formula_1_08(N_obsh_n, f_n, alpha, P):
     return N_obsh_n * f_n * np.sum(alpha * P)
 
+# Ебейшая формула из большой таблицы
+def Giga_Formula(T_ost, delta_t, K_pr, K_reg, K_z, K_cu, X_i, gamma, omega, N_fp, q, delta_KP):
+    E_cp = 125 + 68 * np.log10(((gamma * omega * N_fp * q ** delta_t * q ** T_ost - 1) / (q - 1)) - 1)
+    if T_ost < delta_t:
+        E_f = 108.7*K_pr*K_reg*K_z*K_cu
+        E_tr = (E_cp * K_pr * K_reg * K_z * K_cu)/X_i
+    else:
+        E_i = 125 + 68*(np.log10((gamma * omega * N_fp * q * q**T_ost - q**delta_t)/(q - 1)) - 1)
+        E_f = (E_i * K_pr * K_reg * K_z * K_cu)/X_i
+        if delta_KP > T_ost:
+            E_tr = (E_cp * K_pr * K_reg * K_z * K_cu)/X_i
+        else:
+            E_tr = (E_i * K_pr * K_reg * K_z * K_cu)/X_i
+
+    return E_f, E_tr
