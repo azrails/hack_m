b2727a99564b691a39d918f1b41287d2fa043885
azrail <wsallei@student.21-school.ru>
2021-11-09 18:18:49 +0300

    12

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import parser
 import os, os.path
 import tkinter as tk
 from tkinter import filedialog as fd
+import parser
+import pandas as pd
+import numpy
 
 class MainApp(tk.Tk):
     def __init__(self) ->None:
@@ -9,11 +12,30 @@ class MainApp(tk.Tk):
         self.geometry('{}x{}+{}+{}'.format(round(self.winfo_screenwidth() / 1.5), round(self.winfo_screenheight() / 1.5), (self.winfo_screenwidth()-self.winfo_screenwidth()//2)// 2, (self.winfo_screenheight()-self.winfo_screenheight() // 2)// 2))
         self.title('Рассчет службы дорожного покрытия')
         self.frame = tk.Frame()
-        self.btn_chose_directory = tk.Button(text='Chose directory', width=30, height=15, master=self.frame, command=self.chose_directory).grid(row=0, column=0, padx=10, pady=10)
+        self.dtatasets = None
+        
+        self.mainmenu = tk.Menu(self)
+        self.filemenu = tk.Menu(self.mainmenu, tearoff=0)
+        self.infomenu = tk.Menu(self.mainmenu, tearoff=0)
+        self.filemenu.add_command(label='Открыть...', command=self.chose_directory)
+        self.filemenu.add_command(label='Сохранить...')
+        self.filemenu.add_command(label='Выход')
+        self.infomenu.add_command(label='Помощь')
+        self.infomenu.add_command(label='О программе')
+        self.config(menu=self.mainmenu)
+        self.mainmenu.add_cascade(label='Файл', menu=self.filemenu)
+        self.mainmenu.add_cascade(label='Информация', menu=self.infomenu)
+        
+
+        #self.btn_chose_directory = tk.Button(text='Chose directory', width=30, height=15, master=self.frame, command=self.chose_directory).grid(row=0, column=0, padx=10, pady=10)
         self.frame.pack()
     
     def chose_directory(self):
-        filename = fd.askopenfilename()
+        directoryname = fd.askdirectory()
+        try:
+            self.dtatasets = parser.main_parser(directoryname)
+        except:
+            pass
 
 def main():
     app = MainApp()
