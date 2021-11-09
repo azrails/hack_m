bc46fa79fbadc6aea132821bc074f3fd1eac82ea
Alex <kalgin.alex@mail.ru>
2021-11-09 15:26:22 +0300

    Отформатировал эксели и допатчил парсер под абсолютный путь

--- parser.py
+++ parser.py
@@ -11,58 +11,61 @@ import pandas as pd
 # Ширина проезжей части
 # Среднегодовая интенсивность
 # Состояние покрытия и дорожной одежды
-def road_parcerType1(x):
-    road = r"C:\Users\Alex\Desktop\Hackathon\Дорога "
+def road_parserType1(x):
+    road = r"C:\Users\Alex\PycharmProjects\hack_m\hack\Дорога "
     road += str(x)
     road += r"\1_Ведомость категории дорог.xlsx"
 
     df = pd.read_excel(r'{}'.format(road))
     return df
 
-def road_parcerType2(x):
-    road = r"C:\Users\Alex\Desktop\Hackathon\Дорога "
+def road_parserType2(x):
+    road = r"C:\Users\Alex\PycharmProjects\hack_m\hack\Дорога "
     road += str(x)
     road += r"\2_Ведомость конструкции дорожной одежды.xlsx"
 
     df = pd.read_excel(r'{}'.format(road))
     return df
 
-def road_parcerType3(x):
-    road = r"C:\Users\Alex\Desktop\Hackathon\Дорога "
+def road_parserType3(x):
+    road = r"C:\Users\Alex\PycharmProjects\hack_m\hack\Дорога "
     road += str(x)
-    road += r"5_Ведомость дорожно-климат районирования.xlsx"
+    road += r"\5_Ведомость дорожно-климат районирования.xlsx"
 
     df = pd.read_excel(r'{}'.format(road))
     return df
 
-def road_parcerType4(x):
-    road = r"C:\Users\Alex\Desktop\Hackathon\Дорога "
+def road_parserType4(x):
+    road = r"C:\Users\Alex\PycharmProjects\hack_m\hack\Дорога "
     road += str(x)
     road += r"\9_Ведомость общих данных по дороге.xlsx"
 
     df = pd.read_excel(r'{}'.format(road))
     return df
 
-def road_parcerType5(x):
-    road = r"C:\Users\Alex\Desktop\Hackathon\Дорога "
+def road_parserType5(x):
+    road = r"C:\Users\Alex\PycharmProjects\hack_m\hack\Дорога "
     road += str(x)
     road += r"\11_Ведомость ширины проезжей части.xlsx"
 
     df = pd.read_excel(r'{}'.format(road))
     return df
 
-def road_parcerType6(x):
-    road = r"C:\Users\Alex\Desktop\Hackathon\Дорога "
+def road_parserType6(x):
+    road = r"C:\Users\Alex\PycharmProjects\hack_m\hack\Дорога "
     road += str(x)
     road += r"\19 Ведомость среднегодовой интенсивности и состава движения.xlsx"
 
     df = pd.read_excel(r'{}'.format(road))
     return df
 
-def road_parcerType7(x):
-    road = r"C:\Users\Alex\Desktop\Hackathon\Дорога "
+def road_parserType7(x):
+    road = r"C:\Users\Alex\PycharmProjects\hack_m\hack\Дорога "
     road += str(x)
     road += r"\Ведомость_ состояния покрытия и дорожной одежды.xlsx"
 
     df = pd.read_excel(r'{}'.format(road))
     return df
+
+
+print(road_parserType5(1))
